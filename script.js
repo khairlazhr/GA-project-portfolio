5683efedacd81b0a70c1c55d5c6cd38466c75c05
khairlazhr <khairlazhr@gmail.com>
2021-07-17 08:45:04 +0800

    nested logic

--- script.js
+++ script.js
@@ -1,37 +1,164 @@
+// Starting Phase
 
-//
+
+
+
+
+// Game Phase
 
 // click function to enable player turn
 var turn = 0 // 1st player
-var clicked = false
+var gameOver = false
 // 2D array checking for winner
-// click on tile (need it so it wont be overwritten)
-function tileClick(cell) {
-    if (cell.innerHTML==="") {
-      if (turn === 0) {
-        cell.innerHTML = "<span class='dot-black'></span>";
-        turn = 1;
-      } else {
-        cell.innerHTML = "<span class='dot-white'></span>";
-        turn = 0;
-      }
-    }
-    
+
+function checkWinner() {
+
 }
-// tables keep expanding
+
+//
+
+function checkWinner(cell) {
+
+}
+
+
+// check horizontal
+
+function checkHorizontal(cell) {
+  const row = cell.dataset.row
+  const col = parseInt(cell.dataset.col)
+  for (let i= 1; i < 5; i++) { // checks right on the fifth node
+    var nextCol = (col + i)
+    if (boardCell(row,nextCol) === undefined) {
+      return false
+    }
+    if (cell.dataset.clicked !== boardCell(row,nextCol).dataset.clicked) {
+        return false
+    }
+  }
+  return true 
+}
+
+function checkLeft(cell) {
+  const row = cell.dataset.row
+  const col = parseInt(cell.dataset.col)
+  for (let i= -4; i < 0; i++) {
+    var nextCol = (col + i)
+    if (boardCell(row,nextCol) === undefined) {
+      return false
+    }
+    if (cell.dataset.clicked !== boardCell(row,nextCol).dataset.clicked) {
+        return false
+    }
+  }
+  return true
+}
+
+// function checkDownFifthNode(cell) {
+//   const row = parseInt(cell.dataset.row)
+//   const col = cell.dataset.col
+//   for (let i= 1; i < 5; i++) {
+//     var nextRow = (row + i)
+//     if (boardCell(row,nextCol) === undefined) {
+//       return false
+//     }
+//     if (cell.dataset.clicked !== boardCell(nextRow, col).dataset.clicked) {
+//         return false
+//     }
+//   } 
+//   return true
+// }
+
+
+// selector function for cells
+function boardCell(row, tile) {
+  return document.querySelector("#board").children[row].children[tile]
+}
+
+// check function
+
+function checkForLoop(cell, direction, node) {
+  const row = cell.dataset.row
+  const col = parseInt(cell.dataset.col)
+  switch (direction) {
+    case "left":
+      
+      switch (node){
+        case "fifth":
+          for (let i= -4; i < 0; i++) {
+            toCheckFunction(cell, row, col, i)
+          }
+          return true
+        case "fourth":
+          for (let i= -3; i < 2; i++) {
+            toCheckFunction(cell, row, col, i)
+          }
+          return true
+        case "third":
+          for (let i= -2; i < 3; i++) {
+            toCheckFunction(cell, row, col, i)
+          }
+          return true
+      }
+    case "right":
+      switch (node) {
+        case "fifth":
+          for (let i= 1; i < 5; i++) { // checks right on the fifth node
+            var nextCol = (col + i)
+            if (boardCell(row,nextCol) === undefined) {
+              return false
+            }
+            if (cell.dataset.clicked !== boardCell(row,nextCol).dataset.clicked) {
+              return false
+              }
+            }
+            return true
+      }
+      
+  }
+}
+
+function toCheckFunction(cell, row, col, i) {
+  var nextCol = (col + i)
+  if (boardCell(row,nextCol) === undefined) {
+    return false
+  }
+  if (cell.dataset.clicked !== boardCell(row,nextCol).dataset.clicked) {
+    return false
+  }
+}
+
+// click on tile so the black and white tiles display
+function tileClick(cell) {
+  if (cell.dataset.clicked !== "0") {
+    return
+  } else {
+    if (turn === 0) {
+      cell.innerHTML = "<span class='dot-black'></span>";
+      cell.dataset.clicked = "1"
+      turn = 1;
+    } else {
+      cell.innerHTML = "<span class='dot-white'></span>";
+      cell.dataset.clicked = "2"
+      turn = 0;
+    }
+  }
+}
+
 
 // generate board
 
 
-const board = document.createElement('table')
-board.setAttribute('id', 'board')
-document.querySelector('#board-holder').append(board)
-
 function buildBoard(rows, columns) {
     for (let i = 0; i < rows; i++) {
-        const row = document.createElement("tr");
+        const row = document.createElement("div");
+        row.classList.add('boardRow')
         for (let j = 0; j < columns; j++) {
-          const tile = document.createElement("td");
+          const tile = document.createElement("div");
+          tile.classList.add('boardColumn')
+          tile.setAttribute('data-row',`${i}`)
+          tile.setAttribute('data-col',`${j}`)
+          tile.setAttribute('data-clicked', '0')
           row.appendChild(tile);
         }
         board.appendChild(row);
@@ -44,11 +171,11 @@ document.querySelector('#board').addEventListener('click', function(e) {
   if (e.target !== this) {
     tileClick(e.target)
   }
+  checkForLoop(e.target, "left", "third")
+  console.log(checkForLoop(e.target, "left", "third"))
+})
 
 
-}
-)
-console.log(document.querySelector('#board').children)
 
 // Additional stuff
 
