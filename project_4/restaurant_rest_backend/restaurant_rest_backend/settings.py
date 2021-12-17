2e067c7460c69f7a4b712f1ccf75622b33da82b9
khairlazhr <khairlazhr@gmail.com>
2021-12-17 10:00:21 +0800

    security risk env

--- project_4/restaurant_rest_backend/restaurant_rest_backend/settings.py
+++ project_4/restaurant_rest_backend/restaurant_rest_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-diin2=zc134!kh7$1g6p0=h18yc_-8cv4h&grhmbod87-8n@xg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
