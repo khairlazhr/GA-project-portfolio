ef1420c2da838cce064e6f3a3ba4c11435cb6a5f
khairlazhr <khairlazhr@gmail.com>
2022-05-29 22:24:26 +0800

    update requirements

--- project_4/restaurant_rest_backend/restaurant_rest_backend/settings.py
+++ project_4/restaurant_rest_backend/restaurant_rest_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "cafenacoffee.herokuapp.com"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
